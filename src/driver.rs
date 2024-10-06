a660665fc3295180a77da31a8d20f3128bbc154c
Hussain <huss4in7@outlook.com>
2024-10-07 00:38:06 +0300

    feat(driver): Add local driver support

--- src/driver.rs
+++ src/driver.rs
@@ -6,14 +6,20 @@ use std::{
     sync::Arc,
     time::Duration,
 };
-use thirtyfour::{error::WebDriverResult, DesiredCapabilities, WebDriver};
+use thirtyfour::{
+    error::WebDriverResult, BrowserCapabilitiesHelper, ChromiumLikeCapabilities,
+    DesiredCapabilities, WebDriver,
+};
 use tokio::sync::Notify;
+
+use crate::utils;
 
 pub struct Driver {
     child: Child,
     driver: WebDriver,
     notifier: Arc<Notify>,
     driver_name: String,
+    port: u16,
 }
 
 impl Driver {
@@ -23,55 +29,59 @@ impl Driver {
         driver.quit().await;
     }
 
-    pub async fn new(notifier: Arc<Notify>) -> Self {
-        if let Some((mut child, DriverType(driver_name, port))) = DriverType::new() {
-            log::info!("Starting {}:{}... ⌛", &driver_name, port);
+    async fn new(notifier: Arc<Notify>) -> Self {
+        if let Some((mut child, DriverType(driver_name, ip, port))) = DriverType::new() {
+            log::warn!("Starting {}:{}... ⌛", &driver_name, port);
 
-            match child.stdout(Stdio::null()).stderr(Stdio::null()).spawn() {
+            const SEC: usize = 10;
+
+            match child
+                .stdin(Stdio::null())
+                .stdout(Stdio::null())
+                .stderr(Stdio::null())
+                .spawn()
+            {
                 Ok(mut child) => {
-                    for counter in 0..=10 {
+                    for counter in 0..=SEC {
                         log::debug!("Waiting 1s... ⌛");
 
                         tokio::select! {
                             _ = tokio::time::sleep(Duration::from_secs(1)) => {
                             }
                             _ = notifier.notified() => {
-                                child.kill().unwrap_or_else(|e| log::error!("Failed to kill {} ❌: {}", &driver_name, e));
-                                log::warn!("Shutting {} down... ✅", &driver_name);
-
-                                if driver_name == "docker" {
-                                    if let Err(e) = Command::new("docker")
-                                        .args(["stop", "selenium"])
-                                        .stdout(Stdio::null())
-                                        .stderr(Stdio::null())
-                                        .spawn()
-                                    {
-                                        log::error!("Failed to stop selenium ❌: {}", e)
-                                    }
-                                }
-
+                                Self::kill(&mut child, &driver_name).await;
                                 break;
                             }
                         }
 
-                        let url = format!("http://localhost:{}", port);
+                        let url = format!("http://{}:{}", ip, port);
+                        let mut caps = DesiredCapabilities::chrome();
 
-                        if let Ok(driver) =
-                            WebDriver::new(&url, DesiredCapabilities::chrome()).await
-                        {
-                            log::warn!("Started {} {} ✅", &driver_name, &url);
-                            tokio::time::sleep(Duration::from_secs(2)).await;
-                            return Self {
-                                child,
-                                driver,
-                                notifier,
-                                driver_name,
-                            };
-                        }
+                        match WebDriver::new(&url, caps).await {
+                            Ok(driver) => {
+                                log::info!("Starting {} {} ✅", &driver_name, &url);
+                                tokio::time::sleep(Duration::from_secs(2)).await;
 
-                        if counter > 10 {
-                            log::error!("Failed to start {} after 10s ❌", &driver_name);
-                            break;
+                                return Self {
+                                    child,
+                                    driver,
+                                    notifier,
+                                    driver_name,
+                                    port,
+                                };
+                            }
+                            Err(e) => {
+                                if counter == SEC {
+                                    log::error!(
+                                        "Failed to conntect to {} after {}s ❌:\n{}\n",
+                                        &driver_name,
+                                        SEC,
+                                        e
+                                    );
+
+                                    Self::kill(&mut child, &driver_name).await;
+                                }
+                            }
                         }
                     }
                 }
@@ -84,24 +94,64 @@ impl Driver {
         std::process::exit(1);
     }
 
-    pub async fn quit(mut self) {
+    async fn run(&self) {
+        log::warn!("Window focus... ⌛");
+        self.driver.execute("window.focus();", vec![]).await;
+        log::debug!("Window focus... ✅");
+
+        log::warn!("Navigating to 'http://google.com'... ⌛");
+        self.driver.goto("http://google.com").await;
+        log::debug!("Navigating to 'http://google.com'... ✅");
+
+        log::warn!("Maximizing window... ⌛");
+        self.driver.maximize_window().await;
+        log::debug!("Maximizing window... ✅");
+    }
+
+    async fn quit(mut self) {
+        const TIMEOUT: Duration = Duration::from_secs(100);
+
+        log::warn!("Quitting WebDriver in {}s... ⌛", TIMEOUT.as_secs());
+
         tokio::select! {
-            _ = tokio::time::sleep(Duration::from_secs(100)) => {
+            _ = tokio::time::sleep(TIMEOUT) => {
+                log::warn!("Automatic shutdown ({}s)... ⌛", TIMEOUT.as_secs());
             }
             _ = self.notifier.notified() => {
                 log::warn!("Shutting {} down... ⌛", self.driver_name);
             }
         };
 
-        if let Err(e) = self.driver.quit().await {
-            log::error!("Failed to quit WebDriver ❌: {}", e);
+        tokio::select! {
+            err = self.driver.quit() => {
+                match err {
+                    Ok(_) => {
+                        log::info!("WebDriver has been closed ✅");
+                    }
+                    Err(e) => {
+                        log::error!("Failed to quit WebDriver ❌: {}", e);
+                    }
+                }
+            }
+
+            _ = tokio::time::sleep(Duration::from_secs(5)) => {
+                log::warn!("Forcing shutdown... ⌛");
+            }
+
+            _ = self.notifier.notified() => {
+                log::warn!("Shutting {} down... ⌛", self.driver_name);
+            }
         }
 
-        if let Err(e) = self.child.kill() {
-            log::error!("Failed to kill {} ❌: {}", self.driver_name, e);
+        Self::kill(&mut self.child, &self.driver_name).await;
+    }
+
+    async fn kill(child: &mut Child, driver_name: &str) {
+        if let Err(e) = child.kill() {
+            log::error!("Failed to kill {} ❌: {}", driver_name, e);
         }
 
-        if self.driver_name == "docker" {
+        if driver_name == "docker" {
             if let Err(e) = Command::new("docker")
                 .args(["stop", "selenium"])
                 .stdout(Stdio::null())
@@ -112,59 +162,53 @@ impl Driver {
             }
         }
 
-        log::warn!("Shutting {} down... ✅", self.driver_name);
-    }
-
-    pub async fn run(&self) {
-        log::info!("Navigating to 'http://google.com'... ⌛");
-
-        self.driver.goto("http://google.com").await;
+        log::info!("Shutting {} down... ✅", driver_name);
     }
 }
 
-struct DriverType(String, u16);
+struct DriverType(String, String, u16);
 
 impl DriverType {
     fn new() -> Option<(Command, Self)> {
-        static VALID_DRIVERS: &[&str] = &["docker", "chromedriver", "geckodriver", "edgedriver"];
+        let port_range = 1024..=65535;
 
         match (
             env::var("DRIVER_TYPE"),
+            env::var("DRIVER_HOST"),
+            env::var("DRIVER_ARGS"),
             env::var("DRIVER_PORT")
                 .ok()
                 .and_then(|p| p.parse::<u16>().ok()),
         ) {
-            (Ok(driver), Some(port @ 1024..=65535)) if VALID_DRIVERS.contains(&driver.as_str()) => {
-                let mut child = Command::new(&driver);
+            (Ok(driver_name), host, args, Some(port @ PORT_RANGE)) => {
+                let mut host = host.unwrap_or("localhost".into());
+                let mut child = Command::new(&driver_name);
 
-                if driver == "docker" {
-                    child.args([
-                        "run",
-                        "--rm",
-                        "--name",
-                        "selenium",
-                        "--shm-size=2g",
-                        "-p",
-                        &format!("{}:4444", port),
-                        "-e",
-                        "SE_VNC_NO_PASSWORD=1",
-                        "selenium/standalone-chrome",
-                    ]);
+                if driver_name == "docker" {
+                    host = "localhost".into();
+                    child
+                        .args(["run", "--rm", "--shm-size=2g"])
+                        .args(["--name", "selenium"])
+                        .args(["-p", &format!("{}:4444", port)])
+                        .args(["-e", "SE_VNC_NO_PASSWORD=1"])
+                        .args(["-v", "./user-data:/home/seluser/.config/google-chrome"])
+                        .arg("selenium/standalone-chrome");
                 } else {
+                    if let Ok(args) = args {
+                        child.args(args.split_whitespace());
+                    }
                     child.arg(format!("--port={}", port));
                 }
 
-                return Some((child, Self(driver, port)));
+                return Some((child, Self(driver_name, host, port)));
             }
-            (Ok(driver), Some(port @ 1024..=65535)) => {
+            (_, _, _, Some(port)) => {
                 log::error!(
-                    "DRIVER_TYPE {} is not supported\nUse ('{}')",
-                    driver,
-                    VALID_DRIVERS.join("' | '")
+                    "PORT {} should be between {} and {}",
+                    port,
+                    port_range.start(),
+                    port_range.end()
                 );
-            }
-            (_, Some(port)) => {
-                log::error!("PORT {} should be between 1024 and 65535", port)
             }
             _ => log::error!("DRIVER_TYPE & DRIVER_PORT are missing"),
         };
--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,12 @@
 use inquire::{ui::RenderConfig, Confirm};
+use std::time::Duration;
+use tokio::task;
 
-pub fn confirm_prompt(question: &str) -> bool {
-    static DEFAULT_ASNWER: bool = false;
-
+pub fn confirm_prompt(question: &str, default_answer: Option<bool>) -> bool {
     Confirm {
         message: question,
         starting_input: None,
-        default: None,
+        default: Some(true),
         placeholder: Some("y|n"),
         help_message: None,
         error_message: "Choose 'yes' or 'no'".into(),
@@ -14,7 +14,7 @@ pub fn confirm_prompt(question: &str) -> bool {
             true => "yes".to_owned(),
             false => "no".to_owned(),
         },
-        parser: &|ans| match ans {
+        parser: &|ans| match ans.to_lowercase().as_str() {
             "y" | "yeah" | "yep" | "yes" => Ok(true),
             "n" | "nope" | "nah" | "no" => Ok(false),
             _ => Err(()),
@@ -26,5 +26,5 @@ pub fn confirm_prompt(question: &str) -> bool {
         render_config: RenderConfig::default(),
     }
     .prompt()
-    .unwrap_or(DEFAULT_ASNWER)
+    .unwrap_or(default_answer.unwrap_or(false))
 }
